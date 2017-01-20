ebe933146a898c9c62d6d43ead9c80d659a26988
petalsofcherry <1246000821@qq.com>
2017-01-21 00:38:31 +0800

    Delete the_0004th_problem.py

--- the_0004th_problem.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-import re
-
-class tocount(object):
-    def countword(self, text):
-        pattern = re.compile(r'\W')
-        the_list = re.split(pattern, text)
-        return len(the_list)
-
