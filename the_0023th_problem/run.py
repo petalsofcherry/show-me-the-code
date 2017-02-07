fe186ec121c779955bdde9205e5d7e849d0ee276
petalsofcherry <1246000821@qq.com>
2017-02-07 18:51:56 +0800

    add the 23th

--- the_0023th_problem/run.py
+++ the_0023th_problem/run.py
@@ -3,7 +3,7 @@
 '''
 之前没写过后端的程序，第一次写成这样我已经很开心了（虽然知道很菜
 '''
-from flask import Flask, session, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request
 from flask.ext.wtf import Form
 from wtforms import StringField, TextField, SubmitField
 from wtforms.validators import  DataRequired
@@ -21,9 +21,7 @@ class pulishform(Form):
 def pulish(comment_list = []):
     form = pulishform()
     if request.method == 'POST' and form.validate_on_submit():
-        session['name'] =  form.name.data
-        session['comment'] = form.comment.data
-        comment_list.append(session.get('name') + '：' + session.get('comment'))
+        comment_list.append(form.name.data + '：' + form.comment.data)
         return redirect(url_for('pulish'))
     return render_template('content.html', form=form, comment_list=comment_list)
 
